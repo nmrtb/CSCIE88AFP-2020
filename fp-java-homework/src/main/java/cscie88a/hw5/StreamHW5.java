8284bf75885631693bfcbffed6ea1295981a6b8d
Truman <truman@nexl.io>
2020-03-06 09:35:56 +0100

    Problem 5 done

--- fp-java-homework/src/main/java/cscie88a/hw5/StreamHW5.java
+++ fp-java-homework/src/main/java/cscie88a/hw5/StreamHW5.java
@@ -2,6 +2,12 @@ package cscie88a.hw5;
 
 import cscie88a.hw4.AnimalType;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamHW5 {
@@ -24,9 +30,49 @@ public class StreamHW5 {
     }
 
     public static void getNamesOfHealthyAnimals() {
-        AnimalGenerator.generateStreamOfAnimalsFromCollection(20)
+        AnimalGenerator.generateStreamOfAnimalsFromCollection(20) // 20 per spec
                 .filter(HealthyAnimalFilter.filter)
                 .map(animal -> animal.getName())
                 .forEach(name -> System.out.println(name));
+    }
+
+    public static Optional<StreamAnimal> getOldestAnimal() { //TODO why optional? can I make it always return a StreamAnimal
+        return AnimalGenerator.generateStreamOfAnimalsFromCollection(20) // 20 per spec }
+                .reduce((maxSoFar, nextAnimal) -> maxSoFar.getAge() > nextAnimal.getAge() ? maxSoFar : nextAnimal);    }
+
+    public static Double getAverageAge() {
+        Stream<StreamAnimal> resultStream = AnimalGenerator.generateStreamOfAnimalsFromCollection(20); // 20 per spec }
+
+        Double average = resultStream.reduce(0.0,
+                            (a, b) -> a + b.getAge(),
+                            (a, b) -> a + b);
+        // TODO currently only giving the total
+//        average = average / resultStream.count();
+        // TODO Explain the difference of reduce 2 arg vs 3 arg versions
+        return average;
+    }
+
+    public static List<String> getAnimalNamesAsList() {
+        int number = 20; // 20 per spec
+        Stream<StreamAnimal> resultStream = AnimalGenerator.generateStreamOfAnimalsFromCollection(number);
+        List<String> resultList = resultStream.collect(
+                () -> new ArrayList<>(),
+                (array, item) -> array.add(item.getName()),
+                (item1, item2) -> item1.addAll(item2)
+        );
+
+        return resultList;
+    }
+
+
+    public static Map<AnimalType, Long> getAnimalGroupCounts() {
+        int number = 100; // 20 per spec
+        Stream<StreamAnimal> resultStream = AnimalGenerator.generateStreamOfAnimalsFromCollection(number);
+        Collector<StreamAnimal, ?, Map<AnimalType, Long>> countingAnimalTypes
+                = Collectors.groupingBy(StreamAnimal::getAnimalType, Collectors.counting());
+
+        Map<AnimalType, Long> result = resultStream.collect(countingAnimalTypes);
+
+        return result;
     }
 }
--- fp-java-homework/src/test/java/cscie88a/hw5/StreamHW5Tests.java
+++ fp-java-homework/src/test/java/cscie88a/hw5/StreamHW5Tests.java
@@ -3,6 +3,9 @@ package cscie88a.hw5;
 import cscie88a.hw4.AnimalType;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,5 +31,31 @@ class StreamHW5Tests {
 		StreamHW5.getNamesOfHealthyAnimals();
 	}
 
+	@Test
+	// Calls method for visual checking
+	public void TestGetOldestAnimal(){
+		Optional<StreamAnimal> oldest = StreamHW5.getOldestAnimal();
+		System.out.println(oldest);
+	}
+
+	@Test
+	// Calls method for visual checking
+	public void TestGetAverageAge(){
+		Double average = StreamHW5.getAverageAge();
+		System.out.println(average);
+	}
+
+	@Test
+	// Calls method for visual checking
+	public void TestGetAnimalNamesAsList(){
+		List<String> testList = StreamHW5.getAnimalNamesAsList();
+		System.out.println(testList);
+	}
+	@Test
+	// Calls method for visual checking
+	public void TestGetAnimalGroupCounts(){
+		Map<AnimalType, Long> result = StreamHW5.getAnimalGroupCounts();
+		System.out.println(result);
+	}
 
 }
