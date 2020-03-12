6c01f7b95302d105e56f8b1cc49b53cc7087af87
Truman <truman@nexl.io>
2020-03-12 22:32:10 +1100

    problem 2 done and most of 3

--- fp-scala-homework/src/main/scala/com/cscie88a/week6/FunctionUtils2.scala
+++ fp-scala-homework/src/main/scala/com/cscie88a/week6/FunctionUtils2.scala
@@ -4,4 +4,37 @@ object FunctionUtils2 {
   def prefixLogger(prefix: String): String => String = (str: String) => {
     prefix + " " + str
   }
+
+  def sumInt(inList: List[Int]): Int = {
+    inList.foldRight(0) { (a, b) => a + b }
+  }
+
+  def productInt(inList: List[Int]): Int = {
+    inList.foldRight(1) { (a, b) => a * b }
+  }
+
+  def all(inList: List[Boolean]): Boolean = {
+    inList.foldRight(true) { (a, b) => a && b }
+  }
+
+  def any(inList: List[Boolean]): Boolean = {
+    inList.foldRight(false) { (a, b) => a || b }
+  }
+
+  def tokenize(text: List[String]): List[String] = {
+    for {
+      item <- text
+      token <- item.split(" ")
+    } yield (token)
+  }
+
+  def updateCounts(word: String, counts: Map[String, Int]): Map[String, Int] = {
+    val a = counts getOrElse(word, 1)
+
+    if (counts contains word) {
+      counts + (word -> (a + 1))
+    } else {
+      counts + (word -> a)
+    }
+  }
 }
--- fp-scala-homework/src/test/scala/com/cscie88a/week6/FunctionUtils2Test.scala
+++ fp-scala-homework/src/test/scala/com/cscie88a/week6/FunctionUtils2Test.scala
@@ -6,11 +6,60 @@ final class FunctionUtils2Test
     extends WordSpec
     with Matchers
     with BeforeAndAfterAll {
-    // add your tests below
 
   "prefixLogger" should {
       "concatenate" in {
         FunctionUtils2.prefixLogger("<scala>")("rocks") should be("<scala> rocks")
       }
     }
+
+  "sumInt" should {
+      "sum items in a list" in {
+        FunctionUtils2.sumInt(List(1, 2, 3, 4)) should be(10)
+      }
+    }
+
+  "productInt" should {
+      "multiply items in a list" in {
+        FunctionUtils2.productInt(List(1, 2, 3, 4)) should be(24)
+      }
+    }
+
+  "all" should {
+      "return true if all items are true" in {
+        FunctionUtils2.all(List(true, true)) should be(true)
+      }
+      "return false if not all items are true" in {
+        FunctionUtils2.all(List(true, false)) should be(false)
+      }
+    }
+
+  "any" should {
+      "return true if one item is true" in {
+        FunctionUtils2.any(List(true, false)) should be(true)
+      }
+
+      "return false if no items are true" in {
+        FunctionUtils2.any(List(false, false)) should be(false)
+      }
+    }
+
+  "tokenize" should {
+    val sentence1 = List("I am a list")
+    val sentence2 = List("I am", "a list to tokenize")
+    val sentence3 = List("I", "am", "a", "list")
+    "return a list of strings" in {
+      FunctionUtils2.tokenize(sentence1) should be(List("I", "am", "a", "list"))
+      FunctionUtils2.tokenize(sentence2) should be(List("I", "am", "a", "list", "to", "tokenize"))
+      FunctionUtils2.tokenize(sentence3) should be(List("I", "am", "a", "list"))
+    }
+  }
+  "updateCounts" should {
+    "update counts of word" in {
+      FunctionUtils2.updateCounts("this", Map("this" -> 5, "is" -> 1)) should be(Map("this" -> 6, "is" -> 1))
+    }
+    "adds word if not in map" in {
+      FunctionUtils2.updateCounts("a", Map("this" -> 5, "is" -> 1)) should be(Map("this" -> 5, "is" -> 1, "a" -> 1))
+    }
+  }
 }
