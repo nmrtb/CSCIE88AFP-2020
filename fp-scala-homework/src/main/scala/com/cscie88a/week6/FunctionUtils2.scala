741942fbde0b9eda51b8e286fc22cc88e399896e
Truman <truman@nexl.io>
2020-03-13 11:27:56 +1100

    Finished all problems except the end of problem 3

--- fp-scala-homework/src/main/scala/com/cscie88a/week6/FunctionUtils2.scala
+++ fp-scala-homework/src/main/scala/com/cscie88a/week6/FunctionUtils2.scala
@@ -29,12 +29,131 @@ object FunctionUtils2 {
   }
 
   def updateCounts(word: String, counts: Map[String, Int]): Map[String, Int] = {
-    val a = counts getOrElse(word, 1)
+    val v = counts getOrElse(word, 1)
 
     if (counts contains word) {
-      counts + (word -> (a + 1))
+      counts + (word -> (v + 1))
     } else {
-      counts + (word -> a)
+      counts + (word -> v)
     }
+  }
+
+  def wordCount(text: List[String]): Map[String, Int] = {
+
+    // Below doesn't "increment" if word appears twice
+    val a = for {
+      token <- tokenize(text) // creates list of words for iterating
+      (k, v) <- updateCounts(token, Map()) // creates tuples (word, 1)
+    } yield (k, v)
+
+//    val b = a.foldLeft(Map.empty[String, Int]) { (a, b) => case (acc, (k, v)) =>
+//      acc.updated(k, acc.getOrElse(k, Seq.empty[String]) ++ Seq(v)) }
+
+    val b = a.map( t => t._1 -> t._2)
+    println(a)
+    println(b)
+
+    // Below works, doesn't use updateCounts
+    // https://janzhou.org/2015/scala-functional-programming-for-word-count.html
+//    tokenize(text).groupBy((word:String) => word).mapValues(_.length)
+//    Map("this" -> 1)
+    a.toMap
+  }
+}
+
+case class Student(
+  id: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  gender: String,
+  country: String
+)
+
+object Students {
+  val allStudents: List[Student] = List(
+    Student("1", "Emmy", "Conrart", "econrart0@gizmodo.com", "male", "China"),
+    Student("2", "Marin", "Blasoni", "mblasoni1@edublogs.org", "female", "United States"),
+    Student("3", "Jesse", "Chismon", "jchismon2@hostgator.com", "male", "China"),
+    Student("4", "Delmore", "Scriver", "dscriver3@boston.com", "male", "United States"),
+    Student("5", "Jocelyn", "Blaxlande", "jblaxlande4@europa.eu", "female", "China")
+  )
+
+  val allStudentsMap: Map[String, Student] = Map(
+    "1" -> allStudents(0),
+    "2" -> allStudents(1),
+    "3" -> allStudents(2),
+    "4" -> allStudents(3),
+    "5" -> allStudents(4)
+  )
+
+  def studentNamesByCountry(country: String): List[String] =
+    allStudents
+      .filter(student => student.country == country)
+      .map(student => student.firstName + " " + student.lastName)
+
+  def studentTotalsByCountry(country: String): Int =
+    allStudents.map(item => item.country)
+    .filter(_ == country)
+    .size
+}
+
+case class Subject(
+  id: String,
+  name: String
+)
+
+object Subjects {
+  val allSubjects: Map[Int, Subject] = Map(
+    1 -> Subject("1", "Physics"),
+    2 -> Subject("2", "Chemistry"),
+    3 -> Subject("3", "Math"),
+    4 -> Subject("4", "English")
+  )
+}
+case class Score(
+  studentId: Int,
+  subjectId: Int,
+  numericScore: Int
+)
+
+object Scores {
+  val allScores: List[Score] = List(
+    Score(1, 1, 87),
+    Score(1, 2, 92),
+    Score(2, 1, 82),
+    Score(2, 2, 80),
+    Score(2, 3, 89),
+    Score(2, 4, 85),
+    Score(3, 3, 89),
+    Score(3, 4, 90),
+    Score(4, 1, 89),
+    Score(4, 2, 86),
+    Score(4, 4, 90),
+    Score(5, 2, 90),
+    Score(5, 3, 87),
+    Score(5, 4, 92)
+  )
+
+  def scoresByStudentId(studentId: Int,
+                        scores: List[Score],
+                        subjectMap: Map[Int, Subject])
+                        : Map[String, Int] = {
+
+    scores
+      .filter( score => score.studentId == studentId)
+      .map( score => (subjectMap(score.subjectId).name, score.numericScore))
+      .toMap
+  }
+
+  def scoresBySubjectId(subjectId: Int,
+                        scores: List[Score],
+                        studentMap: Map[String, Student])
+                        : Map[String, Int] = {
+
+    scores
+      .filter( score => score.subjectId == subjectId)
+      .map( score => ( studentMap(score.studentId.toString).firstName + " " + studentMap(score.studentId.toString).lastName, score.numericScore) )
+      .toMap
   }
 }
--- fp-scala-homework/src/test/scala/com/cscie88a/week6/FunctionUtils2Test.scala
+++ fp-scala-homework/src/test/scala/com/cscie88a/week6/FunctionUtils2Test.scala
@@ -48,18 +48,39 @@ final class FunctionUtils2Test
     val sentence1 = List("I am a list")
     val sentence2 = List("I am", "a list to tokenize")
     val sentence3 = List("I", "am", "a", "list")
+    val sentence4 = List("I", "am", " ", "list")
     "return a list of strings" in {
       FunctionUtils2.tokenize(sentence1) should be(List("I", "am", "a", "list"))
       FunctionUtils2.tokenize(sentence2) should be(List("I", "am", "a", "list", "to", "tokenize"))
       FunctionUtils2.tokenize(sentence3) should be(List("I", "am", "a", "list"))
+      FunctionUtils2.tokenize(sentence4) should be(List("I", "am", "list"))
     }
   }
+
   "updateCounts" should {
     "update counts of word" in {
       FunctionUtils2.updateCounts("this", Map("this" -> 5, "is" -> 1)) should be(Map("this" -> 6, "is" -> 1))
     }
+    "update counts with empty Map" in {
+      FunctionUtils2.updateCounts("this", Map()) should be(Map("this" -> 1))
+    }
     "adds word if not in map" in {
       FunctionUtils2.updateCounts("a", Map("this" -> 5, "is" -> 1)) should be(Map("this" -> 5, "is" -> 1, "a" -> 1))
+    }
+  }
+
+  "wordCount" should {
+    val sentence1 = List("I am a list")
+    val map1 = Map("I" -> 1, "am" -> 1, "a" -> 1, "list" -> 1)
+    val sentence2 = List("I am I list")
+    val map2 = Map("I" -> 2, "am" -> 1, "list" -> 1)
+
+    "returns Map if one of each word" in {
+      FunctionUtils2.wordCount(sentence1) should be(map1)
+    }
+
+    "adds to wordcount if word in list" in {
+      FunctionUtils2.wordCount(sentence2) should be(map2)
     }
   }
 }
